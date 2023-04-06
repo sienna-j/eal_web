03edfaeef2b9beee4ccba5254c01fb1581cecafe
sienna <nox.reines@gmail.com>
2023-04-06 11:24:22 +0900

    changed color of nodes, 3-hop nodes are extracted

--- app.py
+++ app.py
@@ -8,10 +8,26 @@ import networkx as nx
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
+def two_hop_edges(output_edges, graph_edges):
+    G = nx.Graph()
+    G.add_edges_from(graph_edges)
+
+    two_hop_nodes = set()
+    for edge in output_edges:
+        two_hop_nodes.update(nx.single_source_shortest_path_length(G, edge[0], cutoff=3).keys())
+        two_hop_nodes.update(nx.single_source_shortest_path_length(G, edge[1], cutoff=3).keys())
+
+    filtered_edges = []
+    for edge in graph_edges:
+        if edge[0] in two_hop_nodes and edge[1] in two_hop_nodes:
+            filtered_edges.append(edge)
+    return np.array(filtered_edges)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    graph_before_data = np.load('static/graph.npy')
+    graph_edges = np.load('static/graph.npy')
     output_edges = np.load('static/output.npy')
+    graph_before_data = two_hop_edges(output_edges, graph_edges)
 
     if request.method == 'POST':
         selected_edges = request.form.getlist('selected_edges')
@@ -27,4 +43,4 @@ def index():
     return render_template("index.html", graph_before_data=graph_before_data.tolist(), output_edges=output_edges.tolist(), new_edges=[], fixed_positions=fixed_positions)
 
 if __name__ == '__main__':
-    app.run(port=4480, debug=True)
+    app.run(port=45554, debug=True)
